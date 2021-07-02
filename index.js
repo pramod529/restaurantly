737338d88eb52e03a51781f4cf6d21c0ba38a17a
pmedim61 <pmedim61@in.ibm.com>
2021-07-02 19:01:41 +0530

    Intial Commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use(express.static(__dirname + "/public"));
+app.get('/', (req, res) => {
+    res.sendFile('./index.html', { root: __dirname });
+});
+app.listen(port, () => console.log(`listening on port ${port}!`));
